915423c29f7b81dadb4a3ccb7cd5f170d52fd64a
Htun Naing <htunhtunnaing.zotefamily@gmail.com>
2022-03-02 23:19:15 +0630

    front-end

---
+++ resources/views/components/fe-footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-sm p-6 bg-gray-900 text-center text-white">
+    Made with Love. © 2022 - 2032 MarioTech
+</footer>
---
+++ resources/views/components/fe-menu.blade.php
@@ -0,0 +1,25 @@
+<div class="grid grid-cols-12 h-16 border-b border-gray-700">
+    <div class="col-start-2 col-span-2 flex items-center">
+        <h1 class="text-3xl">MarioTech</h1>
+    </div>
+
+    <div class="col-start-5 col-span-7">
+        <ul class="flex items-center justify-center h-full">
+            <li>
+                <a href="#" class="text-sm transition ease-in-out duration-300 hover:text-indigo-500 px-4">DevOps</a>
+            </li>
+            <li>
+                <a href="#" class="text-sm transition ease-in-out duration-300 hover:text-indigo-500 px-4">Linux System Administration</a>
+            </li>
+            <li>
+                <a href="#" class="text-sm transition ease-in-out duration-300 hover:text-indigo-500 px-4">PHP</a>
+            </li>
+            <li>
+                <a href="#" class="text-sm transition ease-in-out duration-300 hover:text-indigo-500 px-4">Laravel</a>
+            </li>
+            <li>
+                <a href="#" class="text-sm transition ease-in-out duration-300 hover:text-indigo-500 px-4">Design</a>
+            </li>
+        </ul>
+    </div>
+</div>
