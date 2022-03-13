6d304cd9b4dddb20d444bbaa36aa1cd85527c92b
Htun Naing <htunhtunnaing.zotefamily@gmail.com>
2022-03-13 14:13:54 +0630

    update script

--- scripts/laravel-deploy.sh
+++ scripts/laravel-deploy.sh
@@ -3,7 +3,7 @@ cd /var/www/html
 
 /usr/local/bin/composer install -d /var/www/html
 npm run dev
-cp /var/www/html/.env.example /var/www/html/.env
+# cp /var/www/html/.env.example /var/www/html/.env
 php81 /var/www/html/artisan key:generate
 php81 /var/www/html/artisan migrate
 php81 /var/www/html/artisan storage:link
